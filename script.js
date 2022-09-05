fa47f3167cd3fac260a4f8a9e7331de76ec38564
pvig-ynov <pierre.vigie@ynov.com>
2022-09-05 13:38:13 +0200

    added game function

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
-function getComputerChoice () {
+function getcomputerChoice() {
     var values = ["rock","paper","scisors"];
     return values[Math.floor(Math.random() * values.length)];
     }
 
-    function playround (playerchoice,computerchoice) {
-        if (playerchoice == computerchoice){
-            console.log("égalité")
-        }
-        else if ((playerchoice == computerchoice) || ())
+    function playRound (playerSelection,computerSelection) {
 
+    // rock > scisors
+    // player > computer
+        if ((playerSelection == "rock") &&  (computerSelection == "scisors")){
+            return "you win";
+        }
+    // computer beats player
+        else if ((computerSelection == "rock") && (playerSelection == "scisors")) {
+            return "Computer win";
+        }
+    // scisors > paper
+    // player > computer
+        if ((playerSelection == "scisors") &&  (computerSelection == "paper")){
+            return "you win";
+        }
+    // computer beats player
+        else if ((computerSelection == "scisors") && (playerSelection == "paper")) {
+            return "Computer win";
+        }
+    // paper > rock
+        if ((playerSelection == "paper") &&  (computerSelection == "rock")){
+            return "you win";
+        }
+    // computer beats player
+        else if ((computerSelection == "paper") && (playerSelection == "rock")) {
+            return "Computer win";
+        }
+    // égalité
+        else {
+            return "égalité";
+        }
     }
 
-    const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+   
+function game() {
+    const playerSelection = prompt("enter rock, paper or scisors");
+    for (let i = 0; i < 5; i++) {
+
+        const computerSelection = getcomputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+
+             }
+     
+}
